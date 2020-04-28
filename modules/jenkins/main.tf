99ae60935cfcda1833b28dbe63c24c9d43075ddd
CK <chengkai.liang@bigfishgames.com>
2020-04-28 13:16:04 -0700

    minor update

--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -1,6 +1,6 @@
-data "terraform_remote_state" "services-host" {
+data "terraform_remote_state" "bde-project" {
   backend = "gcs"
-  config {
+  config = {
     bucket      = "bde-tf-state-dev"
     prefix      = "terraform/state"
     credentials = file("~/.config/gcloud/tf-svc-acct.json")
@@ -10,13 +10,13 @@ data "terraform_remote_state" "services-host" {
 module "jenkins" {
   source = "../.."
   credential_file = file("~/.config/gcloud/tf-svc-acct.json")
-  region = data.terraform_remote_state.services-host.region
-  zone = data.terraform_remote_state.service-host.zone
+  region = data.terraform_remote_state.bde-project.region
+  zone = data.terraform_remote_state.bde-project.zone
   gke_host_endpoint = data.terraform_remote_state.service-host.gke_cluster_endpoint
   gke_cluster_ca_certificate = data.terraform_remote_state.service-host.gke_cluster_ca_cert
 
   namespace = "stable/jenkins"
   values = [
-    "${file("${module.path}/files/values.yaml")}"
+    "${file("${path.module}/files/values.yaml")}"
   ]
 }
