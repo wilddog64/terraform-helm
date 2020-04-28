dacc548eda54ede241ffc4b958505b920a535de5
CK <chengkai.liang@bigfishgames.com>
2020-04-28 14:00:20 -0700

    add terraform_remote_state to query bde-gke module

--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -7,13 +7,22 @@ data "terraform_remote_state" "bde-project" {
   }
 }
 
+data "terraform_remote_state" "bde-gke" {
+  backend = "gcs"
+  config = {
+    bucket      = "bde-tf-state-dev"
+    prefix      = "terraform/state"
+    credentials = file("~/.config/gcloud/tf-svc-acct.json")
+  }
+}
+
 module "jenkins" {
   source = "../.."
   credential_file = file("~/.config/gcloud/tf-svc-acct.json")
   region = data.terraform_remote_state.bde-project.outputs.region
   zone = data.terraform_remote_state.bde-project.outputs.zone
-  gke_host_endpoint = data.terraform_remote_state.service-host.gke_cluster_endpoint
-  gke_cluster_ca_certificate = data.terraform_remote_state.service-host.gke_cluster_ca_cert
+  gke_host_endpoint = data.terraform_remote_state.bde-gke.outputs.cluster_endpoint
+  gke_cluster_ca_certificate = data.terraform_remote_state.bde-gke.outputs.cluser_ca_certificate
 
   namespace = "stable/jenkins"
   values = [
