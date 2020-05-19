0f4f807aa62292697ebe6d9eef7e24b34b9623c7
CK <chengkai.liang@bigfishgames.com>
2020-05-18 17:52:55 -0700

    update port-fowarding shell script

--- modules/jenkins/jenkins-port-forwarding.sh
+++ modules/jenkins/jenkins-port-forwarding.sh
@@ -19,10 +19,4 @@
 
 set -o nounset                                  # Treat unset variables as an error
 # gcloud container clusters get-credentials bde --region us-west2 --project services-host-dev-5babb4e8 \
-# && kubectl port-forward --namespace bde-jenkins $(kubectl get pod --namespace bde-jenkins --selector="app.kubernetes.io/component=cjoc,com.cloudbees.cje.tenant=cjoc" --output jsonpath='{.items[0].metadata.name}') 8080:8080
-
-# gcloud container clusters get-credentials bde --region us-west2 --project services-host-dev-5babb4e8 \
-#  && kubectl port-forward --namespace bde-jenkins $(kubectl get pod --namespace bde-jenkins --selector="app.kubernetes.io/component=cloudbees-jenkins-distribution-master,app.kubernetes.io/instance=bde" --output jsonpath='{.items[0].metadata.name}') 8080:8080
-
-gcloud container clusters get-credentials bde --region us-west2 --project services-host-dev-5babb4e8 \
- && kubectl port-forward --namespace bde-jenkins $(kubectl get pod --namespace bde-jenkins --selector="app.kubernetes.io/component=jenkins-master,app.kubernetes.io/instance=bde" --output jsonpath='{.items[0].metadata.name}') 8080:8080
+kubectl port-forward --namespace bde-jenkins $(kubectl get pod --namespace bde-jenkins --selector="app.kubernetes.io/component=jenkins-master,app.kubernetes.io/instance=bde" --output jsonpath='{.items[0].metadata.name}') 8080:8080
