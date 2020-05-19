6aa2b2aaf0caf653584fc377dc889126490f1c54
CK <chengkai.liang@bigfishgames.com>
2020-05-18 17:51:48 -0700

    update backend.tf

--- modules/jenkins/backend.tf
+++ modules/jenkins/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket      = "services-host-3b114438-storage"
+    bucket      = "bde-tf-state-dev"
     prefix      = "terraform/state"
     credentials = "~/.config/gcloud/tf-svc-acct.json"
   }
